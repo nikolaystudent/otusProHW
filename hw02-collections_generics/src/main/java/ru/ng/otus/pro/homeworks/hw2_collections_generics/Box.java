f4dc1ff3328e402ccacd2fcdaafbf1745ec40400
testuser <you@example.com>
2023-09-13 15:08:40 +0500

    Checking homework 2.2

--- hw02-collections_generics/src/main/java/ru/ng/otus/pro/homeworks/hw2_collections_generics/Box.java
+++ hw02-collections_generics/src/main/java/ru/ng/otus/pro/homeworks/hw2_collections_generics/Box.java
@@ -11,23 +11,29 @@ public class Box <T extends Fruit>{
         this.content.addAll(new ArrayList<>(Arrays.asList(fruits)));
     }
 
-    public void inputInBox(ArrayList<T> fruits){
+    public void inputInBox(ArrayList<? extends T> fruits){
         this.content.addAll(fruits);
     }
 
     public float weight(){
         float result = 0.0f;
         for (T i : content) {
-            result = result + i.getWeight();
+            result += i.getWeight();
         }
         return result;
     }
 
     public boolean compare(Box<? extends Fruit> box){
+        if (box == null){
+            return false;
+        }
         return Math.abs(this.weight() - box.weight()) < 0.0001;
     }
 
-    public void moveContent(Box<T> box){
+    public void moveContent(Box<? super T> box){
+        if (box == this || box == null){
+            return;
+        }
         box.inputInBox(content);
         this.content.clear();
     }
